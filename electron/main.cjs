b8b483ac2ce99070bba7ac3e9910433c1e48d611
banmni <hbeen3259@gmail.com>
2026-09-08 20:31:21 -0600

    basics placed

---
+++ electron/main.cjs
@@ -0,0 +1,23 @@
+const {app, BrowserWindow, ipcMain, BrowserView} = require("electron")
+
+const createWindow = ()=>{
+    const win = new BrowserWindow({
+        width: 800,
+        height: 800,
+    })
+
+    win.loadURL("http://localhost:5173")
+}
+
+app.whenReady().then(()=>{
+    createWindow()
+
+     app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
